bff94aedf21292327a0ed29b811f92f4fc020082
tamasPetki <tonertop@gmail.com>
2019-02-20 19:08:53 +0100

    App.JS init version implemented

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const sequelize = require('./util/database');
+const bodyparser = require('body-parser');
+const authRoutes = require('./routes/auth');
+const User = require('./models/user');
+
+app.use(bodyparser.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+    });
+
+app.use(authRoutes);
+
+app.use((req,res,next) => {res.status(404).send('<h1>Page not found</h1>')});
+
+app.use((error, req, res, next) => {
+    const status = error.statusCode || 500;
+    const message = error.message;
+    const data = error.data;
+    res.json({message: message, data: data, status: status});
+});
+
+sequelize.sync()
+    .then(app.listen(process.env.PORT || 8080))
+    .catch(error => console.log(error));
