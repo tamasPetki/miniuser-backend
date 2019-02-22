8ec9e0180acfef0e85e42430b89d1cc6c64aabc0
tamasPetki <tonertop@gmail.com>
2019-02-22 13:11:26 +0100

    isAuth added to route

--- routes/main.js
+++ routes/main.js
@@ -5,6 +5,6 @@ const isAuth = require('../middleware/is-auth');
 
 router.get('/all-users', isAuth, feedController.getAllUsers);
 
-router.delete('/delete-user/:id', isAuth, feedController.deleteUser);
+router.delete('/delete-user/:id', isAuth , feedController.deleteUser);
 
 module.exports = router;
